6b8e9a25122ed50daae1be01070c7f2f5f803812
milandamen <mdamen3@student.avans.nl>
2015-09-26 23:04:58 +0200

    - Background color for currently playing song in playlist - Folder scope visualization in playlist

--- app.js
+++ app.js
@@ -12,6 +12,8 @@ playlist = [];
 function play(index) {
     if (index < playlist.length && index > -1) {
         playlist[index].play();
+        setPlaying(player.currentPlayIndex, false);
+        setPlaying(index, true);
         player.currentPlayIndex = index;
         $('#playerContainer .songName').text(playlist[index].name);
     }
@@ -30,23 +32,41 @@ function playPrevious() {
 /**************************************/
 
 function add(element) {
+    var scope = getScope(element);
+    if (scope !== '') {
+        scope = scope.substring(0, scope.length - 1);
+    } else {
+        scope = '/';
+    }
+
     var song = {
         url: element.dataset.url,
         name: element.textContent,
+        scope: scope,
         play: function() {
             player.src = this.url;
         }
     };
+
+    if (playlist.length === 0 || playlist[playlist.length-1].scope !== scope) {
+        $('#playlist').append(
+            '<li class="d">' +
+                scope +
+            '</li>'
+        );
+    }
+
     playlist.push(song);
-    playIfFirstSong();
 
     $('#playlist').append(
-        '<li>' +
+        '<li class="s">' +
             '<a href="#player" onclick="playSongFromPlaylist(this);" data-index="' + (playlist.length - 1) + '">' +
                 song.name +
             '</a>' +
         '</li>'
     );
+
+    playIfFirstSong();
 }
 
 function addFolder(element) {
@@ -65,4 +85,30 @@ function playIfFirstSong() {
 
 function playSongFromPlaylist(element) {
     play(+element.dataset.index);
+}
+
+function setPlaying(index, bool) {
+    var element = $('#playlist li.s:eq(' + index + ')');
+    if (!element) {return;}
+
+    if (bool) {
+        element.addClass('playing');
+    } else {
+        element.removeClass('playing');
+    }
+}
+
+/*************************************/
+/************* Song Data *************/
+/*************************************/
+
+function getScope(element) {
+    var dir = element.parentNode.parentNode.parentNode;
+
+    if (dir && dir.tagName === 'LI') {
+        var a = dir.childNodes[0];
+        return getScope(a) + a.textContent + '/';
+    }
+
+    return '';
 }
